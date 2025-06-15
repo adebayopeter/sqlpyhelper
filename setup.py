e4e371d2057167d7d4931cf0b228b2866a547ed8
Adebayo Olaonipekun <pekunmi@live.com>
2025-06-16 00:50:20 +0100

    Updated to v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='SQLPyHelper',
-    version='0.1.2',
+    version='0.1.3',
     description='A simple SQL database helper package for Python.',
     long_description=long_description,
     long_description_content_type="text/markdown",
