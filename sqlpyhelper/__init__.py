d331f544847af59770e37a755e8f9251f14ca516
Adebayo Olaonipekun <pekunmi@live.com>
2026-06-04 03:20:17 +0100

    fix: export custom exceptions from __init__.py

--- sqlpyhelper/__init__.py
+++ sqlpyhelper/__init__.py
@@ -1,2 +1,9 @@
 # Match the version in setup.py
-__version__ = "0.1.3"
+__version__ = "0.1.4"
+
+from sqlpyhelper.db_helper import (
+    SQLPyHelperError,
+    ConnectionError,
+    QueryError,
+    BackupError,
+)
